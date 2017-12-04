b5428c4317b2880bc633f540168a034d9480355b
John <jonnygovish@gmail.com>
2017-12-04 09:12:56 +0300

    Create url patterns for driver app

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,8 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from .import views
+from . import views
+
+app_name = 'drivers'
 
 urlpatterns = [
-  
+  url(r'^$',views.driver, name = 'driver'),
+  url(r'^profile/(?P<username>[-_\w.]+)/$', views.profile, name='profile'),
+  url(r'^profile/(?P<username>[-_\w.]+)/edit/$', views.update_profile, name='edit'),
 ]
+
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
