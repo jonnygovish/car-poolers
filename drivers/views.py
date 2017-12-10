1f32e4014a056dc4298e548af18faf25a43d1c9a
John <jonnygovish@gmail.com>
2017-12-10 12:30:42 +0300

    Updating driver views

--- drivers/views.py
+++ drivers/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserForm, ProfileForm
+from .forms import UserForm, DriverProfileForm
 from django.contrib.auth.models import User
 from .models import Driver_profile
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.db import transaction
 
 # Create your views here.
 def driver(request):
-  return render(request, 'driver/driver.html')
+  user = User.objects.get(username = request.user.username)
+  profile = Driver_profile.objects.get(user =user)
+  return render(request, 'driver/driver.html',{"profile": profile})
 
-
+@transaction.atomic
 def update_profile(request,username):
   user = User.objects.get(username = username)
   if request.method == 'POST':
     user_form = UserForm(request.POST, instance = request.user)
-    profile_form = ProfileForm(request.POST, instance =request.user.driver_profile, files = request.FILES)
-    if user_form.is_valid() and profile_form.is_valid():
+    profile_form = DriverProfileForm(request.POST, instance =request.user.driver_profile, files = request.FILES)
+    if profile_form.is_valid() and user_form.is_valid():
       user_form.save()
       profile_form.save()
       messages.success(request, ('Your profile was successfully updated!'))
@@ -26,14 +29,14 @@ def update_profile(request,username):
 
   else:
     user_form = UserForm(instance = request.user)
-    profile_form = ProfileForm(instance = request.user.rider_profile)
+    profile_form = DriverProfileForm(instance = request.user.driver_profile)
   return render(request, 'driver/profiles/profile_form.html', {"user_form":user_form, "profile_form":profile_form})
 
 @login_required
 def profile(request, username):
   user = User.objects.get(username =username)
   if not user:
-    return redirect('rider')
+    return redirect('driver')
   profile = Driver_profile.objects.get(user =user)
   print(profile.profile_pic)
 
