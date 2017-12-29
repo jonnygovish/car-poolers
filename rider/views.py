d09cacce6e30d0eea21acad08bb6ba006c0ea3fc
John <jonnygovish@gmail.com>
2017-12-29 17:16:47 +0300

    Create a view function for bookings

--- rider/views.py
+++ rider/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm,ProfileForm
 from .models import Rider_profile
-from drivers.models import Driver_profile
+from drivers.models import Driver_profile,TripPlan,Booking
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.core.urlresolvers import reverse
@@ -47,13 +47,41 @@ def profile(request, username):
 
 
 #Rider sees a particular driver's profile
-def driver_profile(request,driver_profile_id):  
+def driver_profile(request,driver_profile_id,trip_plan_id):  
   user= User.objects.get(id = driver_profile_id)
   if user:
     driver_profile = Driver_profile.objects.get(user=user)
-  return render(request,'riders/driver_profile.html',{"driver_profile": driver_profile})
+    trip_plan = TripPlan.objects.get(id = trip_plan_id)
+    existing_bookings = Booking.objects.filter(trip_plan =trip_plan.id)
+    if len(existing_bookings) < trip_plan.driver_profile.car_capacity:
+      seats_left = trip_plan.driver_profile.car_capacity - len(existing_bookings)
+      return render(request,'riders/driver_profile.html',{'driver_profile': driver_profile,"seats_left":seats_left,"trip_plan":trip_plan})
+    elif len(existing_bookings) == trip_plan.driver_profile.car_capacity:
+      message = "this ride is fully booked"
+      return render(request,'riders/driver_profile.html',{'driver_profile': driver_profile,"message":message})
 
+def booking_seat(request, driver_profile_id):
+  current_user = request.user
 
+  rider_profile = Rider_profile.objects.get(user= current_user)
+
+  found_profile = Driver_profile.objects.get(id = driver_profile_id)
+
+  trip_plan = TripPlan.objects.get(driver_profile = found_profile)
+
+  existing_bookings = Booking.objects.filter(trip_plan=trip_plan)
+
+  if len(existing_bookings) < trip_plan.driver_profile.car_capacity:
+    new_booking = Booking(rider_profile= rider_profile, trip_plan = trip_plan)
+    
+    new_booking.save()
+
+    print(new_booking)
+
+  elif len(existing_bookings) == trip_plan.driver_profile.car_capacity:
+    return redirect(reverse('rider:driver_profile', kwargs={'driver_profile_id':Driver_profile.user.id}))
+
+  
   
 
 
